bc9e1a27db6788f504f05b3f13ff93869a16dac0
Fabian <13519140@std.stei.itb.ac.id>
2022-03-01 20:53:15 +0700

    weightnya yg penting ga di random bos

--- src/A/main.py
+++ src/A/main.py
@@ -3,7 +3,6 @@ import numpy as np
 
 ffnn = FeedForwardNeuralNetwork()
 ffnn.add_layer(n_neuron=2, activation_function='sigmoid')
-ffnn.add_layer(n_neuron=2, activation_function='sigmoid')
-ffnn.add_layer(n_neuron=1, activation_function='relu')
+ffnn.add_layer(n_neuron=1, activation_function='sigmoid')
 ffnn.predict(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]))
 ffnn.info()
