4ef3d268e31bc705433aa9c918924bccbab08d70
fadelananda <fadelananda123@gmail.com>
2022-03-05 16:41:06 +0700

    split model, add input to model

--- src/A/main.py
+++ src/A/main.py
@@ -1,6 +1,7 @@
 from generate_model import generate_model
 import numpy as np
 
-ffnn = generate_model(filename='model.json')
+model = str(input("Masukkan model yang ingin digunakan [relu / sigmoid]: "))
+ffnn = generate_model(filename=f"{model}.json")
 prediction = ffnn.predict(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]))
 ffnn.info()
