9f11a21f53e2f35e0b56b79581ef520ba6318603
fwznbg <60412952+fwznbg@users.noreply.github.com>
2022-03-26 09:11:07 +0700

    read iris dataset

--- src/B/main.py
+++ src/B/main.py
@@ -1,14 +1,21 @@
 from neural_net import NeuralNetwork
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+dataset = load_iris()
+
+X = dataset.data
+Y = dataset.target
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 NN = NeuralNetwork(max_iter=100, batch_size=2, epoch=3, error_threshold=0.01, learning_rate=0.01)
-NN.add_layer(n_neuron=2)
+NN.add_layer(n_neuron=4)
 NN.add_layer(n_neuron=2, activation_function='relu')
 NN.add_layer(n_neuron=2, activation_function='relu')
 NN.add_layer(n_neuron=1, activation_function='softmax')
 
-x = np.array([[2.1234, 4.1234], [2.134, 3.341], [8.452345, 2.34], [1.23, 3.45], [2.1234, 4.1234], [2.134, 3.341], [8.452345, 2.34], [1.23, 3.45]])
-y = np.array([[0], [0], [1], [1], [0], [0], [1], [1]])
-NN.fit(x, y)
-pred = NN.predict(x)
+NN.fit(X_train, Y_train)
+pred = NN.predict(X_test)
 print(pred)
